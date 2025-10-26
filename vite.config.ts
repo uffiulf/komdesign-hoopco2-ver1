c140d2c2a94d58c4305561feee58c206244041bd
Olav Liljeberg <sofnbeat@gmail.com>
2025-10-26 22:20:48 +0100

    Configure GitHub Pages deployment

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/komdesign-hoopco2-ver1/', // Set this to your repository name
+});
